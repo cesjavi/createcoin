07de62f884438f85a22009f987e767d8e55f88cd
cesjavi <cesjavi@gmail.com>
2025-06-06 11:46:29 -0300

    Improve crear.sh script

--- crear.sh
+++ crear.sh
@@ -1,11 +1,22 @@
+#!/bin/bash
+set -euo pipefail
+
+# Install required dependencies before using them
+sudo apt-get update
+sudo apt-get install -y autoconf make libtool build-essential libboost-all-dev rename
+
+# Clone bitcoin repository and rename
 git clone https://github.com/bitcoin/bitcoin.git
 mv bitcoin tugo
 cd tugo
+
+# Replace occurrences of "bitcoin" with "tugo"
 find ./ -execdir rename 's/bitcoin/tugo/' '{}' \+
 find ./ -type f -exec sed -i 's/bitcoin/tugo/g' {} \;
 find ./ -type f -exec sed -i 's/BITCOIN/TUGO/g' {} \;
 find ./ -type f -exec sed -i 's/Bitcoin/Tugo/g' {} \;
-sudo apt-get install autoconf make libtool build-essential libboost-all-dev rename
+
+# Build the project
 ./autogen.sh
 ./configure
 make -j "$(($(nproc)+1))"
